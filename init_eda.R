55fcdc0f4632beda33d432bff19b95181820fadf
Robert Stallard <robertwstallard@gmail.com>
2020-03-22 22:23:08 -0400

    Update init_eda.R

--- init_eda.R
+++ init_eda.R
@@ -2,6 +2,9 @@
 
 library(tidyverse)
 library(lubridate)
+library(choroplethr)
+library(viridis)
+
 # load time series data from CSSEGIS johns hopkins folks
 # https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series
 
@@ -53,7 +56,7 @@ ky_merged = inner_join(ky_conf, ky_deaths) %>%
   pivot_longer(cols = -date)
 
 ggplot(ky_merged, aes(date, value, fill = name)) + 
-  geom_col(position = 'fill') +
+  geom_col(position = 'dodge') +
   scale_x_date(limits = c(Sys.Date() - 15, NA))
   
 ggplot(ky_merged, aes(date, value, color = name)) + 
@@ -61,6 +64,58 @@ ggplot(ky_merged, aes(date, value, color = name)) +
   scale_x_date(limits = c(Sys.Date() - 15, NA))
 
 
+# confirmed heat map count us
 
+us_confirmed = confirmed %>%
+  rename('state' = `Province/State`) %>%
+  mutate(state = stringr::str_to_lower(state)) %>%
+  inner_join(df_pop_state, by = c('state' = 'region')) %>%
+  rename('state_pop' = value) %>%
+  pivot_longer(cols = c(-state, -`Country/Region`, -Lat, -Long, -state_pop), 
+               names_to = 'date', 
+               values_to = 'confirmed') %>%
+  select(-Lat, -Long) %>%
+  mutate(date = mdy(date)) %>%
+  arrange(date) %>%
+  mutate(confirmed_1000 = confirmed/state_pop/1000)
+
+us_deaths = deaths %>%
+  rename('state' = `Province/State`) %>%
+  mutate(state = stringr::str_to_lower(state)) %>%
+  inner_join(df_pop_state, by = c('state' = 'region')) %>%
+  rename('state_pop' = value) %>%
+  pivot_longer(cols = c(-state, -`Country/Region`, -Lat, -Long, -state_pop), 
+               names_to = 'date', 
+               values_to = 'deaths') %>%
+  select(-Lat, -Long) %>%
+  mutate(date = mdy(date)) %>%
+  arrange(date) %>%
+  mutate(deaths_100000 = deaths/state_pop/100000)
+
+us_recovered = recovered %>%
+  rename('state' = `Province/State`) %>%
+  mutate(state = stringr::str_to_lower(state)) %>%
+  inner_join(df_pop_state, by = c('state' = 'region')) %>%
+  rename('state_pop' = value) %>%
+  pivot_longer(cols = c(-state, -`Country/Region`, -Lat, -Long, -state_pop), 
+               names_to = 'date', 
+               values_to = 'recovered') %>%
+  select(-Lat, -Long) %>%
+  mutate(date = mdy(date)) %>%
+  arrange(date) %>%
+  mutate(recovered_100000 = recovered/state_pop/100000)
+
+ggplot(us_confirmed, aes(date, state, fill = confirmed_1000)) + 
+  geom_tile() + 
+  scale_fill_viridis_c()
+
+ggplot(us_confirmed, aes(date, confirmed, color = state)) + geom_point() + geom_line() +
+  theme(legend.position = 'none') + scale_x_date(limits = c(Sys.Date() - 15, NA))
+
+ggplot(us_deaths, aes(date, deaths, color = state)) + geom_point() + geom_line() +
+  theme(legend.position = 'bottom') + scale_x_date(limits = c(Sys.Date() - 15, NA))
+
+ggplot(us_recovered, aes(date, recovered, color = state)) + geom_point() + geom_line() +
+  theme(legend.position = 'bottom') + scale_x_date(limits = c(Sys.Date() - 15, NA))
 
 
